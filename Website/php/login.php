91df3191bf6e71c08bc1eed9185f5692b6f1567f
Jan Rei A. Sibaen <2160437@slu.edu.ph>
2018-05-14 10:31:30 +0800

    Added login function

--- Website/php/login.php
+++ Website/php/login.php
@@ -1,20 +1,32 @@
 <?php
-session_start();
-if (isset($_POST['login_user'])) {
-    $username = mysqli_real_escape_string($db, $_POST['username']);
-    $password = mysqli_real_escape_string($db, $_POST['password']);
-    $password = hash('sha1', $password);
-    $query = "SELECT * FROM accounts WHERE username='$username' AND password='$password'";
-  	$results = mysqli_query($db, $query);
-  	if (mysqli_num_rows($results) == 1) {
-  	  $_SESSION['username'] = $username;
-  	  $_SESSION['success'] = "You are now logged in";
-  	  header('location: index.php');
-  	}else {
-  		echo "<script>
-        alert('Wrong username/password combination');
-        windows.history.back();
-        <script>";
-  	}
-}
+   $db = mysqli_connect('localhost','root','','webtech');
+   session_start();
+   
+   if($_SERVER["REQUEST_METHOD"] == "POST") {
+      // username and password sent from form 
+       $username = mysqli_real_escape_string($db, $_POST['username']);
+       $password = mysqli_real_escape_string($db, $_POST['password']);
+       $password = hash('sha1', $password);
+      
+      $query = "SELECT * FROM accounts WHERE username = '$username' and password = '$password'";
+      $result = $db->query($query);
+      $row = $result->fetch_assoc();
+      
+      $count = $result->num_rows;
+      
+      // If result matched $myusername and $mypassword, table row must be 1 row
+		
+      if($count == 1) {
+         $_SESSION['login_user'] = $username;
+          echo"<script>
+          alert('You are now logged in');
+          </script>";
+         header("location: ../index.html");
+      }else {
+         echo "<script>
+         alert('Invalid username or password');
+         window.history.back();
+         </script>";
+      }
+   }
 ?>
